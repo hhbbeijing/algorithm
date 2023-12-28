674b60a10135fd4ad40e004f199adb70e88238a5
hanhaibo <hanhaibo@e-sscard.com>
2023-12-28 13:48:59 +0800

    no message

--- algorithm/src/App.vue
+++ algorithm/src/App.vue
@@ -1,21 +1,21 @@
 <!--
- 排序算法：冒泡排序、选择排序、插入排序、希尔排序、归并排序、快速排序、堆排序等。
+    排序算法：冒泡排序、选择排序、插入排序、希尔排序、归并排序、快速排序、堆排序等。
 
-查找算法：顺序查找、二分查找、哈希查找等。
+    查找算法：顺序查找、二分查找、哈希查找等。
 
-字符串匹配算法：暴力匹配、KMP算法、Boyer-Moore算法等。
+    字符串匹配算法：暴力匹配、KMP算法、Boyer-Moore算法等。
 
-图论算法：最短路径算法、最小生成树算法、拓扑排序等。
+    图论算法：最短路径算法、最小生成树算法、拓扑排序等。
 
-动态规划算法：背包问题、最长公共子序列、最长上升子序列等。
+    动态规划算法：背包问题、最长公共子序列、最长上升子序列等。
 
-贪心算法：最小生成树、单源最短路径等。
+    贪心算法：最小生成树、单源最短路径等。
 
-分治算法：快速排序、归并排序等。
+    分治算法：快速排序、归并排序等。
 
-网络流算法：最大流问题、最小割问题等。
+    网络流算法：最大流问题、最小割问题等。
 
-数学算法：欧几里得算法、素数判断、大数计算等
+    数学算法：欧几里得算法、素数判断、大数计算等
  -->
 <script setup lang="ts">
 import {ref} from "vue";
@@ -28,18 +28,18 @@ const result = ref('')
 /**
  * 冒泡排序。
  * 冒泡排序是最简单的排序算法之一，其基本思想是将相邻的两个元素进行比较，如果顺序不对就交换它们的位置。该算法的时间复杂度为O(n^2)。
+ * 升序 - 里面for 若前者 > 后者，交换；每循环一次，外层for长度减1
  */
 const maopaoClickListener = () => {
     let length: number = 4;
     let array: number[] = getArray(length);
     rawData.value = JSON.stringify({...array})
-    for (let i = 0; i < length; i++) {
-        let itemI = array[i]
-        for (let j = i + 1; j < length; j++){
-            let itemJ = array[j]
-            if (itemI > itemJ) {
-                array[j] = itemI
-                array[i] = itemJ
+    for (let i = 0; i < length - 1; i++) {
+        for(let j = 0; j < length - 1 - i; j++) {
+            if(array[j] < array[j+1]){
+                let temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
             }
         }
     }
